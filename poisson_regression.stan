08a93986b51cd4225e0299cba801f05fe54c98a4
John Pearson <jmxpearson@gmail.com>
2023-09-18 16:49:59 -0400

    adding regression variables

---
+++ poisson_regression.stan
@@ -0,0 +1,46 @@
+data {
+  int<lower=0> N; // number of subjects 
+  int<lower=0> R;  // number of regions
+  int<lower=0> D;  // number of latent dimensions
+  int<lower=0> P;  // number of regressors
+  int<lower=0> count[N, R];
+  vector<lower=0>[R] volume;
+  matrix[N, P] beh;
+}
+
+// The parameters accepted by the model. Our model
+// accepts two parameters 'mu' and 'sigma'.
+parameters {
+  matrix[R, D] W;
+  real<lower=0> sigma;
+  vector[R] mu;
+  matrix[D, N] z;
+  matrix[R, N] x;
+  vector<lower=0>[D] alpha;
+  matrix[R, P] beta;
+}
+
+transformed parameters {
+  matrix<lower=0>[R, N] lambda; 
+  
+  lambda = exp(x);
+}
+
+// The model to be estimated. We model the output
+// 'y' to be normally distributed with mean 'mu'
+// and standard deviation 'sigma'.
+model {
+  alpha ~ gamma(1, 10);
+  mu ~ normal(0, 1);
+  to_vector(beta) ~ normal(0, 1);
+  sigma ~ inv_gamma(1, 1);
+  for (k in 1:D) {
+    W[,k] ~ normal(0, alpha[k]);
+  }
+  to_vector(x) ~ normal(to_vector(W * z + rep_matrix(mu, N) + beta * beh'), sigma);
+  for (s in 1:N) {
+    z[,s] ~ normal(0, 1);
+    count[s] ~ poisson(lambda[,s] .* volume);
+  }
+}
+
--- run_stan_model.R
+++ run_stan_model.R
@@ -3,7 +3,7 @@ library(rstan)
 
 dat <- read_delim('data/clean_dat.csv') 
 
-subsample_data <- TRUE
+subsample_data <- FALSE
 subsample_frac <- 0.1
 set.seed(99876)
 
@@ -14,9 +14,10 @@ count <- dat %>% select(id, subject, count) %>%
 volume <- dat %>% select(id, volume) %>% distinct() %>% select(volume) %>%
   pull(volume)
 
-beh <- dat %>% select(id, A, B) %>% distinct() %>% 
+beh <- dat %>% select(subject, A, B) %>% 
   mutate_at(c("A", "B"), function(x) (x - mean(x, na.rm=TRUE))/sd(x, na.rm=TRUE)) %>%
-  replace_na(list(A=0, B=0))
+  replace_na(list(A=0, B=0)) %>% distinct() %>%
+  select(A, B)
 
 
 if (subsample_data) {
@@ -28,19 +29,21 @@ if (subsample_data) {
 N <- dim(count)[1]
 R <- dim(count)[2]
 D <- 5
+P <- 2
 
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
 # run some stan
 nchains <- 4
-stan_dat <- list(N=N, R=R, D=D, count=count, volume=volume)
-pars <- c('mu', 'alpha', 'sigma', "z", "W")
+stan_dat <- list(N=N, R=R, D=D, P=P, count=count, volume=volume, beh=beh)
+pars <- c('mu', 'beta', 'alpha', 'sigma', "z", "W")
 iter <- 2000
 thin <- 1
 stan_seed <- 12345
 
-fit <- stan(file = 'poisson_prob_pca.stan', data=stan_dat, pars=pars, iter=iter,
+file <- 'poisson_regression.stan'
+fit <- stan(file = file, data=stan_dat, pars=pars, iter=iter,
             chains=nchains, thin=thin, seed=stan_seed)
 
 fit_summary <- as.data.frame(summary(fit)$summary) %>% 
@@ -52,4 +55,7 @@ dim(W) <- c(D, R)
 W <- t(W)
 
 mu <- fit_summary %>% filter(str_detect(variable, "mu")) %>% select(mean)
+beta <- fit_summary %>% filter(str_detect(variable, "beta")) %>% select(mean) %>% t()
+dim(beta) <- c(P, R)
+beta <- t(beta)
 
